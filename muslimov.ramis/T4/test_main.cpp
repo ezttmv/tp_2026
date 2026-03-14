2bbdf6d26c75e4b770b033ce79d09504acf86ef3
Ramis <ramis.muslimov1203@yandex.ru>
2026-03-14 14:54:03 +0300

    fix line size in main

--- muslimov.ramis/T4/test_main.cpp
+++ muslimov.ramis/T4/test_main.cpp
@@ -12,20 +12,24 @@ int main() {
     // // ::testing::InitGoogleTest(&argc, argv);
     // // int testResult = RUN_ALL_TESTS();
 
-    // // Демонстрационный вывод — сразу после выполнения тестов
+    // // Демонстрационный вывод —
+    //сразу после выполнения тестов
     // try {
-    //     // Создаём демонстрационный контейнер (в нём минимум 5 фигур, одна составная)
-    //     ShapesContainer container = createDemoContainer();
+    //     // Создаём демонстрационный контейнер
+    //(в нём минимум 5 фигур, одна составная)
+    //     ShapesContainer container =
+    //createDemoContainer();
 
-    //     // Установим формат вывода чисел
+    // Установим формат вывода чисел
     //     std::cout << std::fixed << std::setprecision(2);
 
-    //     // Вывод до масштабирования
+    //Вывод до масштабирования
     //     std::cout << "Before scaling:\n";
     //     const auto &items = container.components();
     //     for (const auto &p : items) {
     //         // Если это составная фигура — печатаем многострочно
-    //         if (const CompositeShape *comp = dynamic_cast<const CompositeShape*>(p.get())) {
+    //         if (const CompositeShape *comp = dynamic_cast<const
+    //CompositeShape*>(p.get())) {
     //             shapes_ui::printComposite(std::cout, *comp);
     //         } else {
     //             shapes_ui::printSimple(std::cout, *p);
