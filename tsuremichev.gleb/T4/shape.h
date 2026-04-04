9cd635d5efaf3a63ff7a769ffc7e54743c5cdab7
mrchudak <gleb.ts2104@gmail.com>
2026-04-04 19:48:23 +0300

    fix(shape): make destructor public

--- tsuremichev.gleb/T4/shape.h
+++ tsuremichev.gleb/T4/shape.h
@@ -2,15 +2,16 @@
 #define SHAPE_H
 #include <string>
 
-class Shape()
+class Shape
 {
+public:
   virtual ~Shape() {}
   virtual double getArea() const = 0;
-  virtual double getCentre() const  = 0;
+  virtual Point getCenter() const  = 0;
   virtual void move(double dx, double dy) = 0;
   virtual void scale(double koef) = 0;
   virtual std::string getName() const = 0;
-}
+};
 
 
 
