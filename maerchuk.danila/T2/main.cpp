bcaacf4becae69d91a6f0bc8775965e26043bc63
Winn-p <maerchuk010107@gmail.com>
2026-05-07 16:17:47 +0300

    5 try T2

--- maerchuk.danila/T2/main.cpp
+++ maerchuk.danila/T2/main.cpp
@@ -10,23 +10,17 @@ int main()
   using nspace::DataStruct;
   std::vector< DataStruct > data;
 
-  // Попытка чтения
-  while (!std::cin.eof())
-  {
-    if (!(std::cin >> std::ws)) break; // Пропуск пробелов
+  std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
 
-    DataStruct temp;
-    if (std::cin >> temp)
+  if (!std::cin.eof() && std::cin.fail())
     {
-      data.push_back(temp);
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    else
-    {
-      if (std::cin.eof()) break;
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-  }
 
   // Сортировка [cite: 6, 10]
   std::sort(data.begin(), data.end());
